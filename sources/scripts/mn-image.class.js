885b8e4188bdd052745139a6a623dece6429ba23
Darlan Mendonça <darlanmendonca@gmail.com>
2016-10-19 11:18:44 -0200

    add click effect to mn-image

--- sources/scripts/mn-image.class.js
+++ sources/scripts/mn-image.class.js
@@ -5,6 +5,7 @@ class MnImage extends HTMLElement {
     this.setOutline();
     this.setRotation();
     this.setBlur();
+    this.setClick();
     return self;
   }
 
@@ -58,7 +59,7 @@ class MnImage extends HTMLElement {
   setRotation() {
     this.addEventListener('mouseenter', setRotation3d);
     this.addEventListener('mousemove', setRotation3d);
-    this.addEventListener('mouseout', unsetRotate3d);
+    this.addEventListener('mouseleave', unsetRotate3d);
 
     function setRotation3d(event) {
       if (this.parentElement.tagName === 'A') {
@@ -101,6 +102,33 @@ class MnImage extends HTMLElement {
 
     function blur() {
       this.blur();
+    }
+  }
+
+  setClick() {
+    if (this.parentElement.tagName === 'A') {
+      this.parentElement.addEventListener('mousedown', clicked);
+      this.parentElement.addEventListener('mouseup', unclicked);
+    }
+
+    let oldTransform;
+
+    function clicked() {
+      let mnImage = this.querySelector('mn-image');
+      oldTransform = mnImage.style.transform;
+      let transform = mnImage
+        .style
+        .transform
+        .replace('scale(1.07)', 'scale(1.03)')
+        .replace(/rotateX(.+)/, 'rotateX(0)')
+        .replace(/rotateY(.+)/, 'rotateY(0)');
+
+      mnImage.style.transform = transform;
+    }
+
+    function unclicked() {
+      let mnImage = this.querySelector('mn-image');
+      mnImage.style.transform = oldTransform;
     }
   }
 }
